df99476f3187ccd2c1cb9198f975db2fcd6ebd0e
Alberto <alberto.doimo.ad@gmail.com>
2025-06-03 18:51:05 +0200

    loaded new sensitivity files

--- measurements/z723/amp_calibration/playback_signal_generation.py
+++ measurements/z723/amp_calibration/playback_signal_generation.py
@@ -19,12 +19,12 @@ fs = 192000 # Hz
 all_sweeps = []
 for durn in durns:
     t = np.linspace(0, durn, int(fs*durn))
-    start_f, end_f = 1e3, 20e3
+    start_f, end_f = 100, 24e3
     sweep = signal.chirp(t, start_f, t[-1], end_f)
-    sweep *= signal.windows.tukey(sweep.size, 0.95)
+    sweep *= signal.windows.tukey(sweep.size, 0.2)
     sweep *= 0.8
     sweep_padded = np.pad(sweep, pad_width=[int(fs*0.1)]*2, constant_values=[0,0])
     all_sweeps.append(sweep_padded)
     
 sweeps_combined = np.concatenate(all_sweeps)
-sf.write('prova.wav', sweeps_combined, samplerate=fs)
+sf.write('01_24k_5sweeps.wav', sweeps_combined, samplerate=fs)
