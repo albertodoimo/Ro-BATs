b3def7394560650e498f391cd5bdb59aa5aab562
Alberto <alberto.doimo.ad@gmail.com>
2025-08-19 18:06:17 +0200

    sync analysis for camera and robot

---
+++ tracking/audio_camera_sync/sync_check.py
@@ -0,0 +1,139 @@
+# %%
+
+import numpy as np 
+import soundfile as sf
+import matplotlib.pyplot as plt 
+from utilities import *
+import scipy.signal as sig
+import csv
+import os
+
+# %%
+# import signature noise signal
+dir = os.path.dirname(os.path.abspath(__file__))
+video_out_signal, fs = sf.read(os.path.join(dir, 'data', 'alternating_white_noise.wav'))
+video_out_signal = video_out_signal[fs*5:fs*10]  # take only one channel
+
+#%% 
+# Plot the time-domain signal and spectrogram
+plt.figure(figsize=(15, 8))
+
+# Time-domain plot
+plt.subplot(2, 1, 1)
+plt.plot(np.linspace(0, len(video_out_signal) / fs, len(video_out_signal)), video_out_signal)
+plt.title('Output Signal')
+plt.xlabel('Time [s]')
+plt.ylabel('Amplitude')
+
+# Spectrogram plot
+plt.subplot(2, 1, 2, sharex=plt.gca())
+plt.specgram(video_out_signal, Fs=fs, NFFT=512, noverlap=256, cmap='viridis')
+plt.title('Spectrogram of the Output Signal')
+plt.xlabel('Time [s]')
+plt.ylabel('Frequency [Hz]')
+plt.ylim(0, 25e3)
+plt.tight_layout()
+plt.show()
+
+# %%
+# import robot audio 
+
+robat_238_raw_audio, fs = sf.read(os.path.join(dir, 'data','134.34.226.238', 'MULTIWAV_134.34.226.238_2025-08-15__16-34-38.wav'))
+
+# cut the signal
+start = 0
+end  = 15
+robat_238_raw_audio = robat_238_raw_audio[fs*start:fs*end,:]
+
+#%% # Plot the time-domain signal and spectrogram
+
+channel_to_show = 2
+robat_238_audio = robat_238_raw_audio[:,channel_to_show]
+plt.figure(figsize=(15, 8))
+
+# Time-domain plot
+plt.subplot(2, 1, 1)
+plt.plot(np.linspace(0, len(robat_238_audio) / fs, len(robat_238_audio)), robat_238_audio, label=f'Mic {channel_to_show+1}')
+plt.title('Robot Audio Signal')
+plt.xlabel('Time [s]')
+plt.ylabel('Amplitude')
+plt.legend()
+
+# Spectrogram plot
+plt.subplot(2, 1, 2, sharex=plt.gca())
+plt.specgram(robat_238_audio, Fs=fs, NFFT=512, noverlap=256, cmap='viridis')
+plt.title('Spectrogram of the Robot Audio Signal')
+plt.xlabel('Time [s]')
+plt.ylabel('Frequency [Hz]')
+plt.ylim(0, 25e3)
+plt.tight_layout()
+plt.show()
+
+# %%
+# correlate the 2 audio files to compare
+
+# Design the highpass filter
+cutoff = 50 # cutoff frequency in Hz
+# Plot the filter frequency response
+sos = signal.butter(2, cutoff, 'hp', fs=fs, output='sos')
+robat_238_audio_hp = sig.sosfilt(sos, robat_238_audio)
+
+#%% 
+#correlate
+filtered_output = np.roll(signal.correlate(robat_238_audio_hp, video_out_signal, mode='same', method='direct'), -len(video_out_signal)//2)
+filtered_envelope = np.abs(signal.hilbert(filtered_output))
+
+# plot the envelope
+plt.figure(figsize=(15, 5))
+plt.plot(np.linspace(0, len(filtered_envelope) / fs, len(filtered_envelope)), filtered_envelope)
+plt.title('Correlation result')
+plt.xlabel('Time [s]')
+plt.ylabel('Amplitude')
+plt.tight_layout()
+plt.show()
+
+peaks, properties = signal.find_peaks(filtered_envelope, prominence=5)
+
+print(f'{np.shape(peaks)} peaks are found at:', peaks / fs, '[s]')
+
+#%%
+# plot the peaks
+plt.figure(figsize=(15, 5))
+plt.plot(np.linspace(0, len(robat_238_audio_hp) / fs, len(robat_238_audio_hp)), robat_238_audio_hp)
+plt.plot(peaks/fs, robat_238_audio_hp[peaks], 'ro')
+plt.title('Detected Peaks')
+plt.xlabel('Time [s]')
+plt.ylabel('Amplitude')
+plt.tight_layout()
+plt.show()
+# %%
+# Load timestamps from file
+timestamps_file = os.path.join(dir, 'data', '134.34.226.238', 'TIMESTAMPS_134.34.226.238_2025-08-15__16-34-38.csv')
+timestamps = []
+with open(timestamps_file, 'r') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        try:
+            # Assuming timestamps are in seconds in the first column
+            # Convert UNIX timestamp to seconds relative to the first timestamp
+            unix_time = float(row[0])
+            if not timestamps:
+                first_unix_time = unix_time
+            timestamps.append(unix_time - first_unix_time + 0.120)
+        except ValueError:
+            continue  
+
+# Overlay timestamps on the audio plot
+plt.figure(figsize=(15, 5))
+plt.plot(np.linspace(0, len(robat_238_audio_hp) / fs, len(robat_238_audio_hp)), robat_238_audio_hp, label='Audio')
+# plt.plot(peaks/fs, robat_238_audio_hp[peaks], 'ro', label='Detected Peaks')
+for ts in timestamps:
+    plt.axvline(ts, color='g', linestyle='--', alpha=0.7, label='Timestamp' if ts == timestamps[0] else "")
+plt.title('Audio with Overlaid Timestamps')
+plt.xlabel('Time [s]')
+plt.ylabel('Amplitude')
+plt.xlim(0, len(robat_238_audio_hp) / fs)   
+plt.legend()
+plt.tight_layout()
+plt.show()
+# %%
---
+++ tracking/audio_camera_sync/utilities.py
@@ -0,0 +1,113 @@
+# -*- coding: utf-8 -*-
+"""
+SPL level from a non-calibration microphone
+===========================================
+See README for broad experimental workflow. 
+
+
+Created April 2025
+@author: thejasvi
+"""
+import numpy as np 
+import scipy.signal as signal 
+from scipy.interpolate import interp1d
+
+def pascal_to_dbspl(X):
+    '''
+    Converts Pascals to dB SPL re 20 uPa
+    '''
+    return dB(X/20e-6)
+
+def rms(X):
+    return np.sqrt(np.mean(X**2))
+
+dB = lambda X: 20*np.log10(abs(np.array(X).flatten()))
+
+db_to_linear = lambda X: 10**(X/20)
+
+
+# bin width clarification https://stackoverflow.com/questions/10754549/fft-bin-width-clarification
+def get_rms_from_fft(freqs, spectrum, **kwargs):
+    '''Use Parseval's theorem to get the RMS level of each frequency component
+    This only works for RFFT spectrums!!!
+    
+    Parameters
+    ----------
+    freqs : (Nfreqs,) np.array >0 values
+    spectrum : (Nfreqs,) np.array (complex)
+    freq_range : (2,) array-like
+        Min and max values
+    
+    Returns 
+    -------
+    root_mean_squared : float
+        The RMS of the signal within the min-max frequency range
+   
+    '''
+    minfreq, maxfreq = kwargs['freq_range']
+    relevant_freqs = np.logical_and(freqs>=minfreq, freqs<=maxfreq)
+    spectrum_copy = spectrum.copy()
+    spectrum_copy[~relevant_freqs] = 0
+    mean_sigsquared = np.sum(abs(spectrum_copy)**2)/spectrum.size
+    root_mean_squared = np.sqrt(mean_sigsquared/(2*spectrum.size-1))
+    return root_mean_squared
+
+
+def calc_native_freqwise_rms(X, fs):
+    '''
+    Converts the FFT spectrum into a band-wise rms output. 
+    The frequency-resolution of the spectrum/audio size decides
+    the frequency resolution in general. 
+    
+    Parameters
+    ----------
+    X : np.array
+        Audio
+    fs : int
+        Sampling rate in Hz
+    
+    Returns 
+    -------
+    fftfreqs, freqwise_rms : np.array
+        fftfreqs holds the frequency bins from the RFFT
+        freqwise_rms is the RMS value of each frequency bin. 
+    '''
+    rfft = np.fft.rfft(X)
+    fftfreqs = np.fft.rfftfreq(X.size, 1/fs)
+    # now calculate the rms per frequency-band
+    freqwise_rms = []
+    for each in rfft:
+        mean_sq_freq = np.sum(abs(each)**2)/rfft.size
+        rms_freq = np.sqrt(mean_sq_freq/(2*rfft.size-1))
+        freqwise_rms.append(rms_freq)
+    return fftfreqs, freqwise_rms
+
+
+# Make an interpolation function 
+def interpolate_freq_response(mic_freq_response, new_freqs):
+    ''' 
+    Parameters
+    ----------
+    mic_freq_response : tuple/list
+        A tuple/list with two entries: (centrefreqs, centrefreq_RMS).
+        
+    new_freqs : list/array-like
+        A set of new centre frequencies that need to be interpolated to. 
+
+    Returns 
+    -------
+    tgtmicsens_interp : 
+        
+    Attention
+    ---------
+    Any frequencies outside of the calibration range will automatically be 
+    assigned to the lowest sensitivity values measured in the input centrefreqs
+    
+    '''
+    centrefreqs, mic_sensitivity = mic_freq_response 
+    tgtmic_sens_interpfn = interp1d(centrefreqs, mic_sensitivity,
+                                    kind='cubic', bounds_error=False,
+                                    fill_value=np.min(mic_sensitivity))
+    # interpolate the sensitivity of the mic to intermediate freqs
+    tgtmicsens_interp = tgtmic_sens_interpfn(new_freqs)
+    return tgtmicsens_interp
